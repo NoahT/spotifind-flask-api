4d607cd71211c540f0f5a45fa288eefaaae6cf48
Noah Teshima <noah.teshima@gmail.com>
2022-07-04 23:12:38 -0700

    /v1/tracks API client implementation

--- src/api/clients/spotify_client/client.py
+++ src/api/clients/spotify_client/client.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
 from ..spotify_auth_client.client import SpotifyAuthClient
+import requests
 
 class Client(ABC):
     @abstractmethod
     def v1_tracks(self, id, marketplace) -> dict:
         pass
-
 
 class SpotifyClient(Client):
     def __init__(self):
@@ -14,7 +14,27 @@ class SpotifyClient(Client):
         self._v1_tracks_path = '/v1/tracks/'
 
     def v1_tracks(self, id, marketplace) -> dict:
-        pass
+        endpoint = '{}{}{}'.format(self._hostname, self._v1_tracks_path, id)
 
+        if marketplace:
+            endpoint = '{}?market={}'.format(endpoint, marketplace)
+
+        bearer_token = self.get_bearer_token()
+
+        headers = {
+            'Authorization': bearer_token
+        }
+
+        response = requests.get(endpoint, headers=headers)
+        response.raise_for_status()
+        response_json = response.json()
+
+        return response_json
+    
     def get_bearer_token(self) -> str:
-        pass
+        bearer_token_json = self._auth_client.get_bearer_token()
+
+        bearer_token = bearer_token_json['access_token']
+        bearer_token = 'Bearer {}'.format(bearer_token)
+        
+        return bearer_token
