a2a8441951b8acb29d64d0706ae9cf41d03bb003
Noah Teshima <noah.teshima@gmail.com>
2022-07-05 00:14:39 -0700

    Implementation of `/v1/tracks` REST API client (#36)
    * Scaffolding for v1/tracks client
    
    * /v1/tracks API client implementation
    
    * Test suite scaffolding for /v1/tracks API client
    
    * Unit tests for /v1/tracks API client

---
+++ src/api/clients/spotify_client/client.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+from ..spotify_auth_client.client import SpotifyAuthClient
+import requests
+
+class Client(ABC):
+    @abstractmethod
+    def v1_tracks(self, id, marketplace) -> dict:
+        pass
+
+class SpotifyClient(Client):
+    def __init__(self):
+        self._auth_client = SpotifyAuthClient()
+        self._hostname = 'https://api.spotify.com'
+        self._v1_tracks_path = '/v1/tracks/'
+
+    def v1_tracks(self, id, **kwargs) -> dict:
+        endpoint = '{}{}{}'.format(self._hostname, self._v1_tracks_path, id)
+
+        marketplace = kwargs.get('marketplace')
+
+        if marketplace:
+            endpoint = '{}?market={}'.format(endpoint, marketplace)
+
+        bearer_token = self.get_bearer_token()
+
+        headers = {
+            'Authorization': bearer_token
+        }
+
+        response = requests.get(endpoint, headers=headers)
+        response.raise_for_status()
+        response_json = response.json()
+
+        return response_json
+    
+    def get_bearer_token(self) -> str:
+        bearer_token_json = self._auth_client.get_bearer_token()
+
+        bearer_token = bearer_token_json['access_token']
+        bearer_token = 'Bearer {}'.format(bearer_token)
+        
+        return bearer_token
---
+++ test/api/clients/spotify_client/test_client.py
@@ -0,0 +1,51 @@
+from unittest.mock import MagicMock, patch
+import unittest
+import src.api.clients.spotify_client.client as spotify_client
+import src.api.clients.spotify_auth_client.client as spotify_auth_client
+import requests
+
+environment_variables = {
+    'PROJECT_ID': '841506577075',
+    'CLIENT_ID': '081f994d972f46519c1c8f9f6f11102a',
+    'SECRET_ID': 'spotify-rest-api-secret',
+    'SECRET_VERSION_ID': 'latest'
+}
+
+class SpotifyClientTestSuite(unittest.TestCase):
+    @patch.dict('os.environ', environment_variables)
+    def setUp(self) -> None:
+        self._spotify_client = spotify_client.SpotifyClient()
+        self._response = requests.Response()
+    
+    def test_should_raise_error_for_4xx_response(self):
+        self._response.status_code = 400
+        requests.get = MagicMock(return_value=self._response)
+        self._spotify_client.get_bearer_token = MagicMock(return_value='Bearer token')
+
+        self.assertRaises(requests.HTTPError, self._spotify_client.v1_tracks, 'id')
+
+    def test_should_raise_error_for_5xx_response(self):
+        self._response.status_code = 500
+        requests.get = MagicMock(return_value=self._response)
+        self._spotify_client.get_bearer_token = MagicMock(return_value='Bearer token')
+
+        self.assertRaises(requests.HTTPError, self._spotify_client.v1_tracks, 'id')
+
+    def test_should_return_json_for_2xx_response(self):
+        self._response.status_code = 200
+        requests.get = MagicMock(return_value=self._response)
+        requests.Response.json = MagicMock(return_value={})
+        self._spotify_client.get_bearer_token = MagicMock(return_value='Bearer token')
+
+        response = self._spotify_client.v1_tracks('id')
+
+        self.assertEquals({}, response)
+
+    def test_should_return_correct_authorization_header(self):
+        spotify_auth_client.SpotifyAuthClient.get_bearer_token = MagicMock(return_value={
+            'access_token': 'token'
+        })
+
+        bearer_token = self._spotify_client.get_bearer_token()
+
+        self.assertEquals('Bearer token', bearer_token)
