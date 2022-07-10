0ec33cb49371e4bf47826209ab7db8d90f77b344
Noah Teshima <noah.teshima@gmail.com>
2022-07-10 13:34:51 -0700

    Scaffolding for logging client

---
+++ test/api/clients/logging_client/test_client.py
@@ -0,0 +1,9 @@
+from unittest.mock import MagicMock, patch
+import unittest
+
+class LoggingClientSuite(unittest.TestCase):
+    def test_should_properly_initialize_logging_client_on_creation(self) -> None:
+        pass
+
+    def test_should_return_logging_client_from_given_name(self) -> None:
+        pass
