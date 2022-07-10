918af57556fef35c6f0a1a3dbd82b32370c36844
Noah Teshima <noah.teshima@gmail.com>
2022-07-09 17:59:12 -0700

    Implemented Kubeflow machine learning pipeline for ANN index deployments (#39)
    * Added Kubeflow ANN index deploy component
    
    * Added Dockerfile for images created for custom Kubeflow components
    
    * Scaffolding for ANN index Kubeflow pipeline
    
    * Continuous deployment changes for images for custom Kubeflow components
    
    * Added venv to .gitignore

---
+++ src/ann_index/components/pipeline_deploy.py
@@ -0,0 +1,66 @@
+import argparse
+import time
+from google.cloud import aiplatform_v1
+
+parser = argparse.ArgumentParser(description='Deploy Vertex AI ANN index')
+parser.add_argument('--peering-range', type=str, help='VPC peering range for ANN index deployment')
+parser.add_argument('--region', type=str, help='Region containing ANN index endpoint deployment')
+parser.add_argument('--project-id', type=str, help='The GCP project ID')
+args = parser.parse_args()
+
+PEERING_RANGE_NAME = args.peering_range
+REGION = args.region
+PROJECT_ID = args.project_id
+
+location = "projects/{}/locations/{}".format(PROJECT_ID, REGION)
+aiplatform_endpoint = "{}-aiplatform.googleapis.com".format(REGION)
+
+index_endpoint_service_client = aiplatform_v1.IndexEndpointServiceClient(client_options=dict(api_endpoint=aiplatform_endpoint))
+index_service_client = aiplatform_v1.IndexServiceClient(client_options=dict(api_endpoint=aiplatform_endpoint))
+
+
+def get_ann_index_endpoint():
+    list_index_endpoints_request = aiplatform_v1.ListIndexEndpointsRequest(parent=location)
+    pager_result = index_endpoint_service_client.list_index_endpoints(request=list_index_endpoints_request)
+
+    index_endpoint = pager_result.index_endpoints[0]
+    return index_endpoint
+
+def get_ann_index():
+    list_indexes_request = aiplatform_v1.ListIndexesRequest(parent=location)
+    indexes_pager_result = index_service_client.list_indexes(request=list_indexes_request)
+    
+    index = indexes_pager_result.indexes[0]
+    return index
+
+def deploy_index(index, index_endpoint):
+    deployed_index = aiplatform_v1.DeployedIndex({
+        "id": "{}-deployed".format(index.display_name),
+        "index": index.name,
+        "display_name": "{}-deployed".format(index.display_name),
+        "reserved_ip_ranges": [
+            PEERING_RANGE_NAME
+        ]
+    })
+    
+    deployed_index_request = aiplatform_v1.DeployIndexRequest(index_endpoint=index_endpoint.name, deployed_index=deployed_index)
+    
+    deployed_index = index_endpoint_service_client.deploy_index(request=deployed_index_request)
+
+    while True:
+        if deployed_index.done():
+            break
+        print("Deploying index...")
+        time.sleep(60)
+    
+    return deploy_index
+
+
+spotifind_index = get_ann_index()
+spotifind_index_endpoint = get_ann_index_endpoint()
+
+deployed_indexes = spotifind_index.deployed_indexes
+
+if not deployed_indexes:
+    deployed_index = deploy_index(spotifind_index, spotifind_index_endpoint)
+    print(deployed_index)
