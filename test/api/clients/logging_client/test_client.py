b9438680a863dcb361ae007205bda36e84f0e0c0
Noah Teshima <noah.teshima@gmail.com>
2022-07-10 13:47:12 -0700

    Unit tests for logging client

--- test/api/clients/logging_client/test_client.py
+++ test/api/clients/logging_client/test_client.py
@@ -1,9 +1,20 @@
 from unittest.mock import MagicMock, patch
 import unittest
+import src.api.clients.logging_client.client as logging_client
+
+environment_variables={
+    'PROJECT_NAME': 'spotifind-api'
+}
 
 class LoggingClientSuite(unittest.TestCase):
-    def test_should_properly_initialize_logging_client_on_creation(self) -> None:
-        pass
+    @patch.dict('os.environ', environment_variables)
+    def setUp(self) -> None:
+        self.client = logging_client.LoggingClient()
 
-    def test_should_return_logging_client_from_given_name(self) -> None:
-        pass
+    def test_should_properly_initialize_logging_client_on_creation(self) -> None:
+        project = self.client.client.project
+        self.assertEquals(project, 'spotifind-api')
+
+    def test_should_return_logger_from_logging_client_from_given_name(self) -> None:
+        logger = self.client.get_logger('name')
+        self.assertEquals(logger.name, 'name')
