13e48ace7ec4d0d9d6d9e4d9787977dc2a9b833a
Noah Teshima <noah.teshima@gmail.com>
2022-09-04 14:43:08 -0700

    Lazy initialization and unit tests from regression

--- src/api/clients/matching_engine_client/client_aggregator.py
+++ src/api/clients/matching_engine_client/client_aggregator.py
@@ -2,16 +2,19 @@ from . import client
 from ...config.config_facade import ConfigFacade
 
 class ClientAggregator():
-    def __init__(self, config_facade: ConfigFacade, mock_match_service_client: client.MockMatchServiceClient, match_service_client: client.MatchServiceClient) -> None:
+    def __init__(self, config_facade: ConfigFacade, mock_match_service_client: client.Client, match_service_client: client.Client) -> None:
         self.config_facade = config_facade
         self.mock_match_service_client = mock_match_service_client
         self.match_service_client = match_service_client
+        self.client = None
     
     def get_client(self) -> client.Client:
-        client = self.mock_match_service_client
-        if self.config_facade.is_match_service_enabled():
-            # If we decide to play around with multi-region deployments,
-            # this needs to be refactored
-            client = self.match_service_client
-        
-        return client
+        if not self.client:
+            if self.config_facade.is_match_service_enabled():
+                # If we decide to play around with multi-region deployments,
+                # this needs to be refactored
+                self.client = self.match_service_client()
+            else:
+                self.client = self.mock_match_service_client()
+            
+        return self.client
--- test/unit_tests/api/clients/matching_engine_client/test_client_aggregator.py
+++ test/unit_tests/api/clients/matching_engine_client/test_client_aggregator.py
@@ -8,17 +8,31 @@ class ClientAggregatorTestSuite(unittest.TestCase):
     @patch('src.api.clients.matching_engine_client.client.MockMatchServiceClient')
     def test_should_return_mock_client_when_match_service_disabled(self, mock_match_service_client, match_service_client, config_facade) -> None:
         config_facade.is_match_service_enabled.return_value = False
+        mock_match_service_client_instance = mock_match_service_client.return_value
         client_aggregator = ClientAggregator(config_facade, mock_match_service_client, match_service_client)
         return_client = client_aggregator.get_client()
         
-        self.assertIs(mock_match_service_client, return_client)
+        self.assertIs(mock_match_service_client_instance, return_client)
 
     @patch('src.api.config.config_facade.ConfigFacade')
     @patch('src.api.clients.matching_engine_client.client.MatchServiceClient')
     @patch('src.api.clients.matching_engine_client.client.MockMatchServiceClient')
     def test_should_return_match_service_client_when_match_service_enabled(self, mock_match_service_client, match_service_client, config_facade) -> None:
         config_facade.is_match_service_enabled.return_value = True
+        match_service_client_instance = match_service_client.return_value
         client_aggregator = ClientAggregator(config_facade, mock_match_service_client, match_service_client)
         return_client = client_aggregator.get_client()
         
-        self.assertIs(match_service_client, return_client)
+        self.assertIs(match_service_client_instance, return_client)
+    
+    @patch('src.api.config.config_facade.ConfigFacade')
+    @patch('src.api.clients.matching_engine_client.client.MatchServiceClient')
+    @patch('src.api.clients.matching_engine_client.client.MockMatchServiceClient')
+    def test_should_not_reinstantiate_client_when_created(self, mock_match_service_client, match_service_client, config_facade) -> None:
+        config_facade.is_match_service_enabled.return_value = False
+        mock_match_service_client_instance = mock_match_service_client.return_value
+        client_aggregator = ClientAggregator(config_facade, mock_match_service_client, match_service_client)
+        client_aggregator.get_client()
+        client_aggregator.get_client()
+
+        self.assertEquals(1, mock_match_service_client.call_count)
