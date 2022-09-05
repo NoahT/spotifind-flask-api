f2e00742362b37b87d7a2fd96f0e26094aae8569
Noah Teshima <noah.teshima@gmail.com>
2022-09-05 12:56:41 -0700

    Response in wrapper object; fixed suppressed 400 response

--- src/api/util/reco_adapter.py
+++ src/api/util/reco_adapter.py
@@ -4,7 +4,7 @@ from urllib.error import HTTPError
 from ..clients.spotify_client.client import Client as SpotifyClient
 from ..clients.logging_client.client import Client as LoggingClient
 from ..clients.matching_engine_client.client_aggregator import ClientAggregator
-from ..schemas.response import ResponseBuilderFactory
+from ..schemas.response import ResponseBuilderFactory, Response
 from google.protobuf.json_format import MessageToDict
 
 class RecoAdapter(ABC):
@@ -20,20 +20,24 @@ class V1RecoAdapter(RecoAdapter):
         # This is the feature space we chose for /v1/reco API. Note that these are the same features in Spotify /v1/audio-features API response
         self.feature_mapping = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
     
-    def get_recos(self, id: str, size: int) -> dict:
+    def get_recos(self, id: str, size: str) -> Response:
         recos_response = None
         recos_dict = None
         try:
-            if size <= 0 or not isinstance(size, int):
-                raise HTTPError(None, HTTPStatus.BAD_REQUEST.value, 'Invalid size', None, None)
+            if not size.isdigit():
+                raise HTTPError(None, HTTPStatus.BAD_REQUEST.value, 'Invalid size type.', None, None)
+            size_int = int(size)
+            if size_int <= 0:
+                raise HTTPError(None, HTTPStatus.BAD_REQUEST.value, 'Unable to handle non-positive reco size.', None, None)
             
             audio_features = self.spotify_client.v1_audio_features(id=id)
             track_embedding = self.get_embedding(audio_features=audio_features)
             recos = self.match_service_client.get_match(match_request={'query': track_embedding})
             recos_dict = MessageToDict(recos, including_default_value_fields=True, preserving_proto_field_name=False)
-            recos_response = self.response_builder_factory.get_builder(status_code=HTTPStatus.OK.value).build_response(recos_dict=recos_dict, id=id, size=size)
+            recos_response = self.response_builder_factory.get_builder(status_code=HTTPStatus.OK.value).build_response(recos_response=recos_dict, id=id, size=size)
         except HTTPError as http_error:
-            recos_response = self.response_builder_factory.get_builder(status_code=http_error.code).build_response(recos_dict=recos_dict, id=id, size=size)
+            print(http_error.msg)
+            recos_response = self.response_builder_factory.get_builder(status_code=http_error.code).build_response(recos_response=recos_dict, id=id, size=size)
         
         return recos_response
     
