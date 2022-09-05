a11d61b12d1b58806fafa76d36bd149d206d3110
Noah Teshima <noah.teshima@gmail.com>
2022-09-04 19:40:49 -0700

    REST API client for /v1/audio-features

--- src/api/clients/spotify_client/client.py
+++ src/api/clients/spotify_client/client.py
@@ -8,10 +8,15 @@ class Client(ABC):
     def v1_tracks(self, id, marketplace) -> dict:
         pass
 
+    @abstractmethod
+    def v1_audio_features(self, id) -> dict:
+        pass
+
 class SpotifyClient(Client):
     def __init__(self, auth_client: SpotifyAuthClient, logging_client: LoggingClient):
         self._hostname = 'https://api.spotify.com'
         self._v1_tracks_path = '/v1/tracks/'
+        self._v1_audio_features_path = '/v1/audio-features/'
         self._auth_client = auth_client
         self.logger = logging_client.get_logger(self.__class__.__name__)
 
@@ -27,6 +32,26 @@ class SpotifyClient(Client):
         if marketplace:
             batch.log('marketplace={}'.format(marketplace), severity='INFO')
             endpoint = '{}?market={}'.format(endpoint, marketplace)
+
+        bearer_token = self.get_bearer_token()
+
+        headers = {
+            'Authorization': bearer_token
+        }
+
+        response = requests.get(endpoint, headers=headers)
+        batch.commit()
+        response.raise_for_status()
+        response_json = response.json()
+
+        return response_json
+    
+    def v1_audio_features(self, id) -> dict:
+        batch = self.logger.batch()
+
+        batch.log('Making GET call to /v1/audio-features', severity='INFO')
+        batch.log('track_id={}'.format(id), severity='INFO')
+        endpoint = '{}{}{}'.format(self._hostname, self._v1_audio_features_path, id)
 
         bearer_token = self.get_bearer_token()
 
--- test/unit_tests/api/clients/spotify_client/test_client.py
+++ test/unit_tests/api/clients/spotify_client/test_client.py
@@ -1,7 +1,6 @@
-from unittest.mock import Mock, MagicMock, patch
+from unittest.mock import Mock, patch
 import unittest
 import src.api.clients.spotify_client.client as spotify_client
-import src.api.clients.spotify_auth_client.client as spotify_auth_client
 import requests
 
 environment_variables = {
@@ -20,27 +19,51 @@ class SpotifyClientTestSuite(unittest.TestCase):
         self._spotify_client = spotify_client.SpotifyClient(auth_client, logging_client)
         self._response = requests.Response()
     
-    def test_should_raise_error_for_4xx_response(self):
+    def test_should_raise_error_for_4xx_response_on_v1_tracks(self):
         self._response.status_code = 400
         requests.get = Mock(return_value=self._response)
         self._spotify_client.get_bearer_token = Mock(return_value='Bearer token')
 
         self.assertRaises(requests.HTTPError, self._spotify_client.v1_tracks, 'id')
 
-    def test_should_raise_error_for_5xx_response(self):
+    def test_should_raise_error_for_5xx_response_on_v1_tracks(self):
         self._response.status_code = 500
         requests.get = Mock(return_value=self._response)
         self._spotify_client.get_bearer_token = Mock(return_value='Bearer token')
 
         self.assertRaises(requests.HTTPError, self._spotify_client.v1_tracks, 'id')
 
-    def test_should_return_json_for_2xx_response(self):
+    def test_should_return_json_for_2xx_response_on_v1_tracks(self):
         self._response.status_code = 200
         requests.get = Mock(return_value=self._response)
         requests.Response.json = Mock(return_value={})
         self._spotify_client.get_bearer_token = Mock(return_value='Bearer token')
 
         response = self._spotify_client.v1_tracks('id')
+
+        self.assertEqual({}, response)
+    
+    def test_should_raise_error_for_4xx_response_on_v1_audio_features(self):
+        self._response.status_code = 400
+        requests.get = Mock(return_value=self._response)
+        self._spotify_client.get_bearer_token = Mock(return_value='Bearer token')
+
+        self.assertRaises(requests.HTTPError, self._spotify_client.v1_audio_features, 'id')
+
+    def test_should_raise_error_for_5xx_response_on_v1_audio_features(self):
+        self._response.status_code = 500
+        requests.get = Mock(return_value=self._response)
+        self._spotify_client.get_bearer_token = Mock(return_value='Bearer token')
+
+        self.assertRaises(requests.HTTPError, self._spotify_client.v1_audio_features, 'id')
+
+    def test_should_return_json_for_2xx_response_on_v1_audio_features(self):
+        self._response.status_code = 200
+        requests.get = Mock(return_value=self._response)
+        requests.Response.json = Mock(return_value={})
+        self._spotify_client.get_bearer_token = Mock(return_value='Bearer token')
+
+        response = self._spotify_client.v1_audio_features('id')
 
         self.assertEqual({}, response)
 
