d2eed32464e058a91b8c01c5a5b480ebf5cbf7e0
Noah Teshima <noah.teshima@gmail.com>
2022-11-27 19:29:36 -0800

    Integration tests for Spotify authentication client

--- test/integration_tests/api/clients/spotify_auth_client/test_client.py
+++ test/integration_tests/api/clients/spotify_auth_client/test_client.py
@@ -1,6 +1,8 @@
 import unittest
+from unittest.mock import Mock
 from src.api.clients.spotify_auth_client.client import SpotifyAuthClient
 from src.api.clients.logging_client.client import LoggingClient
+from requests import HTTPError
 
 class SpotifyClientTestSuite(unittest.TestCase):
     def setUp(self) -> None:
@@ -8,7 +10,12 @@ class SpotifyClientTestSuite(unittest.TestCase):
         self.auth_client = SpotifyAuthClient(logging_client=logging_client)
     
     def test_should_return_bearer_token_for_valid_basic_authorization(self) -> None:
-        pass
+        bearer_token_dict = self.auth_client.get_bearer_token()
+        self.assertIsNotNone(bearer_token_dict)
+        self.assertIsNotNone(bearer_token_dict['access_token'])
+        self.assertEqual('Bearer', bearer_token_dict['token_type'])
+        self.assertIsNotNone(bearer_token_dict['expires_in'])
 
     def test_should_not_return_bearer_token_for_invalid_basic_authorization(self) -> None:
-        pass
+        self.auth_client.get_basic_token = Mock(return_value='invalid_basic_token')
+        self.assertRaises(HTTPError, self.auth_client.get_bearer_token)
