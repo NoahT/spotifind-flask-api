4a648557a22da0c81d90cf2bc45a134ec6fb3fcd
Noah Teshima <noah.teshima@gmail.com>
2022-12-05 23:01:25 -0800

    Refactor authentication service client to store Bearer tokens in memory (#79)
    * Integrate TTL Bearer token cache with authorization client
    
    * Refactor references on reco blueprint to prevent re-instantiating for each service call
    
    * Change environemnt to development to verify CD pipeline run
    
    * Revert "Change environemnt to development to verify CD pipeline run"
    
    This reverts commit a8f3783827299936e499ce99826efe566df0f94b.
    
    * Set token cache TTL to one hour

--- src/api/app.py
+++ src/api/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, Blueprint
-from .routes.reco import reco
 from .routes.healthcheck import healthcheck
 from flasgger import Swagger
 import json
@@ -11,17 +10,23 @@ def load_config(app: Flask, config_env: str):
     print('Config file: {}'.format(config_file))
     app.config.from_file(config_file, load=json.load)
 
-v1 = Blueprint('name', __name__)
+def load_blueprints(app: Flask):
+    from .routes.reco import reco
+
+    v1 = Blueprint('name', __name__)
+    swagger = Swagger(app)
+
+    v1.register_blueprint(reco, url_prefix='/reco')
+    app.register_blueprint(v1, url_prefix='/v1')
+
+    # For health checks on Ingress load balancer in GCP
+    app.register_blueprint(healthcheck)
+
 flask_app = Flask(__name__)
+flask_app.app_context().push()
 
 load_config(flask_app, 'default')
 config_env = os.environ['ENVIRONMENT']
 load_config(flask_app, config_env)
 
-swagger = Swagger(flask_app)
-
-v1.register_blueprint(reco, url_prefix='/reco')
-flask_app.register_blueprint(v1, url_prefix='/v1')
-
-# For health checks on Ingress load balancer in GCP
-flask_app.register_blueprint(healthcheck)
+load_blueprints(flask_app)
--- src/api/routes/reco.py
+++ src/api/routes/reco.py
@@ -1,6 +1,5 @@
 from http import HTTPStatus
 from flask import Blueprint, request
-from flasgger import swag_from
 from ..config.config_facade import ConfigFacade
 from ..clients.matching_engine_client.client_aggregator import ClientAggregator
 from ..clients.matching_engine_client.client import MockMatchServiceClient, MatchServiceClient
@@ -12,130 +11,25 @@ from ..util.reco_adapter import V1RecoAdapter
 
 reco = Blueprint('reco', __name__)
 
-@reco.route('/<id>', methods=['GET'])
-@swag_from({
-    "parameters": [
-        {
-            "name": "size",
-            "in": "query",
-            "type": "int",
-            "required": False,
-            "default": "5"
-        },
-        {
-            "name": "id",
-            "in": "path",
-            "type": "string",
-            "required": True
-        }
-    ],
-    "definitions": {
-        "response": {
-            "type": "object",
-            "properties": {
-                "request": {
-                    "$ref": "#/definitions/request_200"
-                },
-                "recos": {
-                    "type": "array",
-                    "items": {
-                        "$ref": "#/definitions/track"
-                    }
-                }
-            }
-        },
-        "request_200": {
-            "description": "/id 200 response",
-            "type": "object",
-            "properties": {
-                "track": {
-                    "$ref": "#/definitions/track"
-                },
-                "size": {
-                    "type": "integer"
-                }
-            }
-        },
-        "request_4xx": {
-            "description": "/id 4xx response",
-            "type": "object",
-            "properties": {
-                "status": {
-                    "type": "integer"
-                },
-                "message": {
-                    "type": "string"
-                }
-            }
-        },
-        "track": {
-            "description": "Model object for Spotify track metadata",
-            "type": "object",
-            "properties": {
-                "id": {
-                    "type": "string"
-                }
-            }
-        }
-    },
-    "responses": {
-        "200": {
-            "description": "A list of track ids.",
-            "schema": {
-                "$ref": "#/definitions/response"
-            },
-            "examples": {
-                "Happy path": {
-                    "summary": "Happy path/valid track id",
-                    "value": {
-                        "request": {
-                            "track": {
-                                "id": "62BGM9bNkNcvOh13B4wOyr"
-                            },
-                            "size": 5
-                        },
-                        "recos": [
-                            {
-                                "id": "2TRu7dMps7cVKOyazkj9Fb"
-                            },
-                            {
-                                "id": "0bqrFwY1HixfnusFxhYbDl"
-                            },
-                            {
-                                "id": "4BHSjbYylfOH5WAGusDyni"
-                            },
-                            {
-                                "id": "3s9f1LQ6607eDj9UYCzmgk"
-                            },
-                            {
-                                "id": "2HbKqm4o0w5wEeEFXm2sD4"
-                            },
-                        ]
-                    }
-                }
-            }
-        }
-    }
-})
+response_builder_factory = ResponseBuilderFactory()
+
+config_facade = ConfigFacade()
+print('Environment: {}'.format(config_facade.get_environment()))
+print('Is match service enabled: {}'.format(config_facade.is_match_service_enabled()))
+
+logging_client = LoggingClient()
+spotify_auth_client = SpotifyAuthClient(logging_client=logging_client)
+spotify_client = SpotifyClient(auth_client=spotify_auth_client, logging_client=logging_client)
+client_aggregator = ClientAggregator(config_facade=config_facade, mock_match_service_client=MockMatchServiceClient, match_service_client=MatchServiceClient)
+reco_adapter = V1RecoAdapter(spotify_client=spotify_client, logging_client=logging_client, client_aggregator=client_aggregator, response_builder_factory=response_builder_factory)
+
 def id(id):
     response = None
     size = request.args.get(key='size') or str(5)
 
-    response_builder_factory = ResponseBuilderFactory()
     try:
-        config_facade = ConfigFacade()
-        print('Environment: {}'.format(config_facade.get_environment()))
-        print('Is match service enabled: {}'.format(config_facade.is_match_service_enabled()))
-
-        logging_client = LoggingClient()
-        spotify_auth_client = SpotifyAuthClient(logging_client=logging_client)
-        spotify_client = SpotifyClient(auth_client=spotify_auth_client, logging_client=logging_client)
-        client_aggregator = ClientAggregator(config_facade=config_facade, mock_match_service_client=MockMatchServiceClient, match_service_client=MatchServiceClient)
-        reco_adapter = V1RecoAdapter(spotify_client=spotify_client, logging_client=logging_client, client_aggregator=client_aggregator, response_builder_factory=response_builder_factory)
-        
-        
         response = reco_adapter.get_recos(id=id, size=size)
-    except Exception as exception:
+    except Exception:
         response = response_builder_factory.get_builder(status_code=HTTPStatus.INTERNAL_SERVER_ERROR.value).build_response(recos_response=None, id=id, size=size)
     finally:
         print(response.__str__())
