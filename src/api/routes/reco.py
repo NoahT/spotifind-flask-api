5c0be8d2e3465293fe91835ab2522253ad360f8a
Noah Teshima <noah.teshima@gmail.com>
2023-01-13 00:58:33 -0800

    Dependency injection for config into REST clients

--- src/api/routes/reco.py
+++ src/api/routes/reco.py
@@ -20,8 +20,8 @@ print('Environment: {}'.format(config_facade.get_environment()))
 print('Is match service enabled: {}'.format(config_facade.is_match_service_enabled()))
 
 logging_client = LoggingClient()
-spotify_auth_client = SpotifyAuthClient(logging_client=logging_client)
-spotify_client = SpotifyClient(auth_client=spotify_auth_client, logging_client=logging_client)
+spotify_auth_client = SpotifyAuthClient(logging_client=logging_client, config_facade=config_facade)
+spotify_client = SpotifyClient(auth_client=spotify_auth_client, logging_client=logging_client, config_facade=config_facade)
 client_aggregator = ClientAggregator(config_facade=config_facade, mock_match_service_client=MockMatchServiceClient, match_service_client=MatchServiceClient)
 reco_adapter = V1RecoAdapter(spotify_client=spotify_client, logging_client=logging_client, client_aggregator=client_aggregator, response_builder_factory=response_builder_factory)
 
