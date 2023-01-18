1d230a89dc876db12371e0662d6546076159578a
Noah Teshima <noah.teshima@gmail.com>
2023-01-17 22:48:52 -0800

    REST client for POST::/v1/users/{user_id}/playlists API

--- test/unit_tests/api/clients/spotify_client/test_client.py
+++ test/unit_tests/api/clients/spotify_client/test_client.py
@@ -71,6 +71,27 @@ class SpotifyClientTestSuite(unittest.TestCase):
         response = self._spotify_client.v1_audio_features('id')
 
         self.assertEqual({}, response)
+    
+    def test_should_return_json_for_2xx_response_on_v1_create_playlist(self):
+        self._response.status_code = 200
+        requests.post = Mock(return_value=self._response)
+        requests.Response.json = Mock(return_value={})
+        
+        response = self._spotify_client.v1_create_playlist('user_id', 'user_token')
+
+        self.assertEqual({}, response)
+    
+    def test_should_raise_error_for_4xx_response_on_v1_create_playlist(self):
+        self._response.status_code = 400
+        requests.post = Mock(return_value=self._response)
+
+        self.assertRaises(requests.HTTPError, self._spotify_client.v1_create_playlist, 'user_id', 'user_token')
+    
+    def test_should_raise_error_for_5xx_response_on_v1_create_playlist(self):
+        self._response.status_code = 500
+        requests.post = Mock(return_value=self._response)
+
+        self.assertRaises(requests.HTTPError, self._spotify_client.v1_create_playlist, 'user_id', 'user_token')
 
     def test_should_return_correct_authorization_header(self):
         auth_client = Mock()
