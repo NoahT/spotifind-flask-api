5c68e987783452e718f3e76ad14ac987061c8215
Noah Teshima <noah.teshima@gmail.com>
2023-01-20 00:39:54 -0800

    Decoupled environment-based config strategy from Flask application context

--- src/api/app.py
+++ src/api/app.py
@@ -1,20 +1,10 @@
 from flask import Flask, Blueprint
 from .routes.healthcheck import healthcheck
-from flasgger import Swagger
-import json
-import os
-
-CONFIG_PATH = './config/'
-def load_config(app: Flask, config_env: str):
-    config_file = '{}{}.json'.format(CONFIG_PATH, config_env)
-    print('Config file: {}'.format(config_file))
-    app.config.from_file(config_file, load=json.load)
 
 def load_blueprints(app: Flask):
     from .routes.reco import reco
 
     v1 = Blueprint('name', __name__)
-    swagger = Swagger(app)
 
     v1.register_blueprint(reco, url_prefix='/reco')
     app.register_blueprint(v1, url_prefix='/v1')
@@ -24,9 +14,5 @@ def load_blueprints(app: Flask):
 
 flask_app = Flask(__name__)
 flask_app.app_context().push()
-
-load_config(flask_app, 'default')
-config_env = os.environ['ENVIRONMENT']
-load_config(flask_app, config_env)
 
 load_blueprints(flask_app)
--- test/unit_tests/api/config/test_config_facade.py
+++ test/unit_tests/api/config/test_config_facade.py
@@ -1,39 +1,44 @@
+import json
 import unittest
+from unittest.mock import mock_open, patch
 from src.api.config.config_facade import ConfigFacade
-import flask
+
+config = {
+    'ENVIRONMENT': 'development',
+    'MATCH_SERVICE_ENABLED': False,
+    'SPOTIFY_AUTH_CLIENT_CONFIG': {
+        'CONNECT_TIMEOUT': 0.500,
+        'READ_TIMEOUT': 1.000
+    },
+    'SPOTIFY_CLIENT_CONFIG': {
+        'CONNECT_TIMEOUT': 0.500,
+        'READ_TIMEOUT': 0.500
+    }
+}
+
 
 class ConfigFacadeTestSuite(unittest.TestCase):
-    def setUp(self) -> None:
-        flask_app = flask.Flask(__name__)
-        flask_app.config['MATCH_SERVICE_ENABLED'] = True
-        flask_app.config['ENVIRONMENT'] = 'development'
-        flask_app.config['SPOTIFY_AUTH_CLIENT_CONFIG'] = {
-            "CONNECT_TIMEOUT": 0.500,
-            "READ_TIMEOUT": 1.000
-        }
-        flask_app.config['SPOTIFY_CLIENT_CONFIG'] = {
-            "CONNECT_TIMEOUT": 0.500,
-            "READ_TIMEOUT": 1.000
-        }
-        with flask_app.app_context():
-            self.config_facade = ConfigFacade()
-
+    @patch('builtins.open', new_callable=mock_open, read_data=json.dumps(config))
+    @patch('os.environ', { 'ENVIRONMENT': 'development' })
+    def setUp(self, mock_file) -> None:
+        self.config_facade = ConfigFacade()
+    
     def test_should_return_environment_when_in_config(self) -> None:
         self.assertEqual('development', self.config_facade.get_environment())
 
     def test_should_return_if_match_service_client_enabled(self) -> None:
-        self.assertTrue(self.config_facade.is_match_service_enabled())
-    
+        self.assertFalse(self.config_facade.is_match_service_enabled())
+
     def test_should_return_spotify_auth_client_config(self) -> None:
         spotify_auth_client_config = self.config_facade.get_spotify_auth_client_config()
 
         self.assertIsNotNone(spotify_auth_client_config)
         self.assertEqual(0.500, spotify_auth_client_config["CONNECT_TIMEOUT"])
         self.assertEqual(1.000, spotify_auth_client_config["READ_TIMEOUT"])
-    
+
     def test_should_return_spotify_auth_client_config(self) -> None:
         spotify_client_config = self.config_facade.get_spotify_client_config()
 
         self.assertIsNotNone(spotify_client_config)
         self.assertEqual(0.500, spotify_client_config["CONNECT_TIMEOUT"])
-        self.assertEqual(1.000, spotify_client_config["READ_TIMEOUT"])
+        self.assertEqual(0.500, spotify_client_config["READ_TIMEOUT"])
