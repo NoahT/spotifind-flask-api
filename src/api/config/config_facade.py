3631180636128ab0aca32c8285ef9b4f61a235a8
Noah Teshima <noah.teshima@gmail.com>
2023-01-20 00:48:24 -0800

    Decoupled environment-based config strategy from Flask application context (#111)

--- src/api/config/config_facade.py
+++ src/api/config/config_facade.py
@@ -1,13 +1,28 @@
-import flask
+import json
+import os
+
+CONFIG_PATH = './config/'
+def load_config(config_env: str):
+    config_file_path = '{}{}.json'.format(CONFIG_PATH, config_env)
+    print('Config file: {}'.format(config_file_path))
+    
+    config = {}
+    with open(config_file_path, 'r', encoding='utf-8') as file:
+        config = json.loads(file.read())
+    
+    return config
 
 class ConfigFacade():
     def __init__(self) -> None:
-        app = flask.current_app
-        with app.app_context():
-            self.environment = flask.current_app.config['ENVIRONMENT']
-            self.match_service_enabled = flask.current_app.config['MATCH_SERVICE_ENABLED']
-            self.spotify_auth_client_config = flask.current_app.config['SPOTIFY_AUTH_CLIENT_CONFIG']
-            self.spotify_client_config = flask.current_app.config['SPOTIFY_CLIENT_CONFIG']
+        config_default = load_config('default')
+        config_environment = load_config(os.environ['ENVIRONMENT'])
+        # https://peps.python.org/pep-0448/
+        config = {**config_default, **config_environment}
+        
+        self.environment = config['ENVIRONMENT']
+        self.match_service_enabled = config['MATCH_SERVICE_ENABLED']
+        self.spotify_auth_client_config = config['SPOTIFY_AUTH_CLIENT_CONFIG']
+        self.spotify_client_config = config['SPOTIFY_CLIENT_CONFIG']
     
     def get_environment(self) -> str:
         return self.environment
