859ba7ab6643a15105ff9c927652d4817c58476e
Noah Teshima <noah.teshima@gmail.com>
2023-01-22 06:07:17 -0800

    Fixed constructor based dependency injection during smoke test

--- src/api/clients/spotify_client/__init__.py
+++ src/api/clients/spotify_client/__init__.py
@@ -3,4 +3,4 @@ import src.api.clients.logging_client as logging_client
 import src.api.clients.spotify_auth_client as spotify_auth_client
 import src.api.config as config
 
-spotify_client = SpotifyClient(spotify_auth_client, logging_client.logging_client, config.config)
+spotify_client = SpotifyClient(spotify_auth_client.spotify_auth_client, logging_client.logging_client, config.config)
