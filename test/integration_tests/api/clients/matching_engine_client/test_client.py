8cb4e77b5f6567e5e1dd319c1510af84ae5ef4ef
Noah Teshima <noah.teshima@gmail.com>
2023-01-26 18:41:20 -0800

    Add missing import from integration tests

--- test/integration_tests/api/clients/matching_engine_client/test_client.py
+++ test/integration_tests/api/clients/matching_engine_client/test_client.py
@@ -1,9 +1,11 @@
 import unittest
 from src.api.clients.matching_engine_client.client import MatchServiceClient
+from src.api.config.config_facade import ConfigFacade
 
 class MatchingEngineClientTestSuite(unittest.TestCase):
     def setUp(self) -> None:
-        self.match_service_client = MatchServiceClient()
+        config_facade = ConfigFacade()
+        self.match_service_client = MatchServiceClient(config_facade)
     
     def test_should_return_match_response_for_singular_reco(self) -> None:
         match_request = {
