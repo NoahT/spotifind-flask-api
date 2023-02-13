cd9a3da905d8a73d3606f30ed4ce1aeedf839b39
Noah Teshima <noah.teshima@gmail.com>
2023-02-12 17:33:27 -0800

    Added integration tests for 4xx use cases on `POST::/v1/users/{user_id}/playlists` API (#137)
    * Stubbing for new integration test cases
    
    * Integration tests for 4xx cases on POST::/v1/users/{user_id}/playlists API client

--- test/integration_tests/api/clients/spotify_client/test_client.py
+++ test/integration_tests/api/clients/spotify_client/test_client.py
@@ -29,35 +29,36 @@ class SpotifyClientTestSuite(unittest.TestCase):
     with flask_app.app_context():
       config_facade = ConfigFacade()
       auth_client = SpotifyAuthClient(config_facade=config_facade)
-      self.spotify_client = SpotifyClient(auth_client=auth_client,
-                                          logging_client=logging_client,
-                                          config_facade=config_facade)
+      self._spotify_client = SpotifyClient(auth_client=auth_client,
+                                           logging_client=logging_client,
+                                           config_facade=config_facade)
+      self._auth_client = auth_client
 
   def test_should_return_response_for_valid_track_id_on_v1_tracks(self) -> None:
-    response = self.spotify_client.v1_tracks('62BGM9bNkNcvOh13B4wOyr')
+    response = self._spotify_client.v1_tracks('62BGM9bNkNcvOh13B4wOyr')
     self.assertIsNotNone(response)
 
   # pylint: disable-next=line-too-long
   def test_should_return_response_for_valid_track_id_and_marketplace_on_v1_tracks(
       self) -> None:
-    response = self.spotify_client.v1_tracks('62BGM9bNkNcvOh13B4wOyr',
-                                             marketplace='US')
+    response = self._spotify_client.v1_tracks('62BGM9bNkNcvOh13B4wOyr',
+                                              marketplace='US')
     self.assertIsNotNone(response)
 
   def test_should_raise_error_for_invalid_track_id_on_v1_tracks(self) -> None:
-    self.assertRaises(exceptions.HTTPError, self.spotify_client.v1_tracks,
+    self.assertRaises(exceptions.HTTPError, self._spotify_client.v1_tracks,
                       'invalid_track_id')
 
   def test_should_raise_error_for_invalid_bearer_token_on_v1_tracks(
       self) -> None:
-    self.spotify_client.get_bearer_token = Mock(
+    self._spotify_client.get_bearer_token = Mock(
         return_value='invalid_bearer_token')
-    self.assertRaises(exceptions.HTTPError, self.spotify_client.v1_tracks,
+    self.assertRaises(exceptions.HTTPError, self._spotify_client.v1_tracks,
                       '62BGM9bNkNcvOh13B4wOyr')
 
   def test_should_return_response_for_valid_track_id_on_v1_audio_features(
       self) -> None:
-    response = self.spotify_client.v1_audio_features('62BGM9bNkNcvOh13B4wOyr')
+    response = self._spotify_client.v1_audio_features('62BGM9bNkNcvOh13B4wOyr')
     self.assertIsNotNone(response)
     self.assertIsNotNone(response['danceability'])
     self.assertIsNotNone(response['energy'])
@@ -74,12 +75,28 @@ class SpotifyClientTestSuite(unittest.TestCase):
   def test_should_raise_error_for_invalid_track_id_on_v1_audio_features(
       self) -> None:
     self.assertRaises(exceptions.HTTPError,
-                      self.spotify_client.v1_audio_features, 'invalid_track_id')
+                      self._spotify_client.v1_audio_features,
+                      'invalid_track_id')
 
   def test_should_raise_error_for_invalid_bearer_token_on_v1_audio_features(
       self) -> None:
-    self.spotify_client.get_bearer_token = Mock(
+    self._spotify_client.get_bearer_token = Mock(
         return_value='invalid_bearer_token')
     self.assertRaises(exceptions.HTTPError,
-                      self.spotify_client.v1_audio_features,
+                      self._spotify_client.v1_audio_features,
                       '62BGM9bNkNcvOh13B4wOyr')
+
+  def test_should_raise_error_for_invalid_bearer_token_on_v1_create_playlist(
+      self) -> None:
+    self.assertRaises(exceptions.HTTPError,
+                      self._spotify_client.v1_create_playlist, 'noahteshima',
+                      'invalid_token')
+
+  def test_should_raise_error_for_wrong_token_scope_on_v1_create_playlist(
+      self) -> None:
+    # public, read-only scope - will cause 403 due to insufficient permission
+    token = self._auth_client.get_bearer_token()
+    token = f'Bearer {token}'
+    self.assertRaises(exceptions.HTTPError,
+                      self._spotify_client.v1_create_playlist, 'noahteshima',
+                      token)
