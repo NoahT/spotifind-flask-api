8ff41b07e6f818ac4b023180c5d7f116b7ea9f4d
Noah Teshima <noah.teshima@gmail.com>
2023-02-20 02:20:24 -0800

    Integration tests for POST::/v1/playlist/{user_id}/{track_id}.

--- test/integration_tests/routes/test_playlist_id.py
+++ test/integration_tests/routes/test_playlist_id.py
@@ -1,23 +1,35 @@
 """ Integration test module for playlist resources. """
 import unittest
 from src.api.app import flask_app
-from werkzeug.test import TestResponse
+from src.api.clients.spotify_auth_client import spotify_auth_client
 
 
 class PlaylistResourceTestSuite(unittest.TestCase):
   """ Integration test suite for POST::v1/{user_id}/{track_id} API. """
 
   def setUp(self) -> None:
-    self._client = flask_app.test_client()
+    self._spotifind_client = flask_app.test_client()
+    self._spotify_auth_client = spotify_auth_client
     self._user_id = 'nteshima'
     self._track_id = '6AUlMVr80H8KGVTGeJlpbp'
     self._uri = f'/v1/playlist/{self._user_id}/{self._track_id}'
 
   def test_should_return_400_for_invalid_size(self) -> None:
-    pass
+    response = self._spotifind_client.post(f'{self._uri}?size=0')
+
+    self.assertEqual(400, response.status_code)
 
   def test_should_return_401_for_invalid_authorization_header(self) -> None:
-    pass
+    headers = {'Authorization': 'Invalid token'}
+
+    response = self._spotifind_client.post(self._uri, headers=headers)
+
+    self.assertEqual(401, response.status_code)
 
   def test_should_return_403_for_insufficient_authorization_scope(self) -> None:
-    pass
+    token_bearer = self._spotify_auth_client.get_bearer_token()
+    headers = {'Authorization': f'Bearer {token_bearer}'}
+
+    response = self._spotifind_client.post(self._uri, headers=headers)
+
+    self.assertEqual(403, response.status_code)
